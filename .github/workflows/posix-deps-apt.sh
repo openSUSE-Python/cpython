6345573d9265339170249f50cd67bf09fc2a54a4
Matěj Cepl <mcepl@cepl.eu>
2024-05-17 01:19:32 +0200

    Convert main branch GitHub Action for OpenSUSE 3.6 builds.

---
+++ .github/workflows/posix-deps-apt.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+zypper refresh
+
+# autoconf-archive is needed by autoreconf (check_generated_files job)
+zypper -nq install \
+    build \
+    pkg-config \
+    autoconf-archive \
+    ccache \
+    gdb \
+    lcov \
+    libb2-devel \
+    libbz2-devel \
+    libffi-devel \
+    gdbm-devel \
+    # libgdbm-compat-devel \
+    xz-devel \
+    mpdecimal-devel \
+    ncurses5-devel \
+    eadline6-devel \
+    sqlite3-devel \
+    libopenssl-1_1-devel \
+    liblzma5 \
+    strace \
+    tk-devel \
+    uuid-devel \
+    xvfb \
+    zlib-devel
