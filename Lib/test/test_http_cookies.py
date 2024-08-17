cdffb9f1bec386198a9fc7483a8f1ad343f48ce2
Serhiy Storchaka <storchaka@gmail.com>
2024-08-17 16:30:52 +0300

    [CVE-2024-7592] Fix quadratic complexity in parsing "-quoted cookie
    Fix quadratic complexity in parsing ``"``-quoted cookie values
    with backslashes by `http.cookies`.
    
    Fixes: gh#python/cpython#123067
    Fixes: bsc#1229596 (CVE-2024-7592)
    From-PR: gh#python/cpython!123075
    Co-authored-by: Serhiy Storchaka <storchaka@gmail.com>
    Patch: CVE-2024-7592-quad-complex-cookies.patch

--- Lib/test/test_http_cookies.py
+++ Lib/test/test_http_cookies.py
@@ -1,7 +1,7 @@
 # Simple test suite for http/cookies.py
 
 import copy
-from test.support import run_unittest, run_doctest, check_warnings
+from test.support import run_unittest, run_doctest, check_warnings, requires_resource
 import unittest
 from http import cookies
 import pickle
@@ -66,6 +66,43 @@ class CookieTests(unittest.TestCase):
             self.assertEqual(C.output(sep='\n'), case['output'])
             for k, v in sorted(case['dict'].items()):
                 self.assertEqual(C[k].value, v)
+
+    def test_unquote(self):
+        cases = [
+            (r'a="b=\""', 'b="'),
+            (r'a="b=\\"', 'b=\\'),
+            (r'a="b=\="', 'b=='),
+            (r'a="b=\n"', 'b=n'),
+            (r'a="b=\042"', 'b="'),
+            (r'a="b=\134"', 'b=\\'),
+            (r'a="b=\377"', 'b=\xff'),
+            (r'a="b=\400"', 'b=400'),
+            (r'a="b=\42"', 'b=42'),
+            (r'a="b=\\042"', 'b=\\042'),
+            (r'a="b=\\134"', 'b=\\134'),
+            (r'a="b=\\\""', 'b=\\"'),
+            (r'a="b=\\\042"', 'b=\\"'),
+            (r'a="b=\134\""', 'b=\\"'),
+            (r'a="b=\134\042"', 'b=\\"'),
+        ]
+        for encoded, decoded in cases:
+            with self.subTest(encoded):
+                C = cookies.SimpleCookie()
+                C.load(encoded)
+                self.assertEqual(C['a'].value, decoded)
+
+    @requires_resource('cpu')
+    def test_unquote_large(self):
+        n = 10**6
+        for encoded in r'\\', r'\134':
+            with self.subTest(encoded):
+                data = 'a="b=' + encoded*n + ';"'
+                C = cookies.SimpleCookie()
+                C.load(data)
+                value = C['a'].value
+                self.assertEqual(value[:3], 'b=\\')
+                self.assertEqual(value[-2:], '\\;')
+                self.assertEqual(len(value), n + 3)
 
     def test_load(self):
         C = cookies.SimpleCookie()
