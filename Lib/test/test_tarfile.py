b3010129c3110570f2e000fe70dca79a2e396275
Matěj Cepl <mcepl@cepl.eu>
2025-06-26 16:40:33 +0200

    avoid possible infinite loop in specifically crafted tarball
    Fixes: bsc#1174091
    Fixes: CVE-2019-20907
    Requires: 1bf5b73dd4c86b8e7125393eee137617a527380d
    Patch: CVE-2019-20907_tarfile-inf-loop.patch

--- Lib/test/test_tarfile.py
+++ Lib/test/test_tarfile.py
@@ -297,6 +297,14 @@ class Bz2ListTest(Bz2Test, ListTest):
 class LzmaListTest(LzmaTest, ListTest):
     pass
 
+    def test_length_zero_header(self):
+        # bpo-39017 (CVE-2019-20907): reading a zero-length header should fail
+        # with an exception
+        with self.assertRaisesRegex(tarfile.ReadError, "file could not be opened successfully"):
+            with tarfile.open(support.findfile('recursion.tar')) as tar:
+                pass
+
+
 
 class CommonReadTest(ReadTest):
 
